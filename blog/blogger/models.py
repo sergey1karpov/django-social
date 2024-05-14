83dc23979c3342c64e863c4754bbccd21f2dc5dc
Karpov Sergey <karpovsergey1990@gmail.com>
2024-05-14 13:51:57 +0400

    Create Post model

--- blog/blogger/models.py
+++ blog/blogger/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+# from django.db import models
+#
+#
+# class BlogPost(models.Model):
+#     title = models.CharField(max_length=255, null=True, blank=True)
+#     short_description = models.TextField(max_length=255, null=True, blank=True)
+#     full_description = models.TextField(max_length=2500, null=True, blank=True)
+#
+#     likes = models.IntegerField(default=0)
+#     reposts = models.IntegerField(default=0)
+#
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     class Meta:
+#         ordering = '-created_at'
+#         verbose_name = 'Blog Post'
+#         verbose_name_plural = 'Blog'
+#         indexes = [
+#             models.Index(fields=["title", "short_description"]),
+#         ]
+#
+#     def __str__(self):
+#         return self.title
+#
+#
+# class BlogPostPhotos(models.Model):
+#     post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='blog_photos')
+#     photo_description = models.CharField(max_length=255, null=True, blank=True)
+#     photo = models.ImageField(upload_to='images/', null=True, blank=True)
 
-# Create your models here.
+
